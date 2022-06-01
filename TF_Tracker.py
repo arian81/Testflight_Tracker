61ed6e137737787b1af3709d1a054b8593436704
arian81 <35879206+arian81@users.noreply.github.com>
2022-06-01 15:57:10 -0400

    Update TF_Tracker.py

--- TF_Tracker.py
+++ TF_Tracker.py
@@ -1,52 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
 from time import sleep
-import asyncio
 
-testflight_id = input(
-    "Enter the testflight id or link to the testflight beta program    >   ")
-CHAT_ID = int(
-    input("Enter the user id recivied from t.me/userinfobot     >     "))
-BOT_TOKEN = input(
-    "Enter the bot token recived from t.me/botfather        >     ")
-if len(testflight_id) == 8:
-    url = "https://testflight.apple.com/join/" + testflight_id
+testflight_id = input("Enter the testflight id or link to the testflight beta program    >   ")
+CHAT_ID = int(input("Enter the user id recivied from t.me/userinfobot     >     ")) 
+BOT_TOKEN = input("Enter the bot token recived from t.me/botfather        >     ")
+if len(testflight_id)==8:
+	url = "https://testflight.apple.com/join/" + testflight_id
 else:
-    url = testflight_id
-
-# while True:
-#     site = requests.get(url)
-#     soup = BeautifulSoup(site.content, "html.parser")
-#     beta_status = soup.find("div", class_="beta-status").span.text
-#     if beta_status != "This beta is full.":
-#         BOT_URL = "https://api.telegram.org/bot{}/sendMessage".format(
-#             BOT_TOKEN)
-#         message = "You can finally join the beta. Hooooray. \b" + url
-#         requests.get(BOT_URL, params={"chat_id": CHAT_ID, "text": message,
-#                      "parse_mode": "html", "disable_web_page_preview": "true"})
-#     else:
-#         print("Sadly the beta program is still full. Patience is a virtue so wait a bit longer. ")
-#     sleep(5)
-
-
-async def track():
+	url = testflight_id
+while True:
     site = requests.get(url)
-    soup = BeautifulSoup(site.content, "html.parser")
-    beta_status = soup.find("div", class_="beta-status").span.text
+    soup = BeautifulSoup(site.content,"html.parser")
+    beta_status = soup.find("div",class_="beta-status").span.text
     if beta_status != "This beta is full.":
-        BOT_URL = "https://api.telegram.org/bot{}/sendMessage".format(
-            BOT_TOKEN)
+        BOT_URL = "https://api.telegram.org/bot{}/sendMessage".format(BOT_TOKEN)
         message = "You can finally join the beta. Hooooray. \b" + url
-        requests.get(BOT_URL, params={"chat_id": CHAT_ID, "text": message,
-                     "parse_mode": "html", "disable_web_page_preview": "true"})
+        requests.get(BOT_URL, params={"chat_id": CHAT_ID, "text": message,"parse_mode": "html", "disable_web_page_preview": "true"})
     else:
         print("Sadly the beta program is still full. Patience is a virtue so wait a bit longer. ")
-    await asyncio.sleep(5)
+    sleep(5)
 
-
-# def start_track():
-#     loop = asyncio.get_event_loop()
-#     loop.create_task(track())
-
-
-asyncio.run(track())
